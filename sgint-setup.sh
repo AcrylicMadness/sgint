65a27596bbe5bd9f6c056e0710f29c8f7732d79a
Acrylic M. <akirill1942@me.com>
2026-02-02 19:24:03 +0300

    Added basic setup script

---
+++ sgint-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+RD=${PWD}
+git clone https://github.com/AcrylicMadness/SwiftGodot-Integrator
+cd SwiftGodot-Integrator
+swift build --configuration release
+BIN_PATH="$(swift build --show-bin-path)"
+echo ${PWD}
+echo $RD
+cp $BIN_PATH/sgint $RD/sgint
+cd $RD
+rm -rf "SwiftGodot-Integrator"
