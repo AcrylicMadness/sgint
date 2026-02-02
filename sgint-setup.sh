fa973f9b263de2f7719888a979ba7fad26877df5
AcrylicMadness <akirill1942@me.com>
2026-02-02 19:27:39 +0300

    Update BIN_PATH to use release configuration

--- sgint-setup.sh
+++ sgint-setup.sh
@@ -4,7 +4,7 @@ RD=${PWD}
 git clone https://github.com/AcrylicMadness/SwiftGodot-Integrator
 cd SwiftGodot-Integrator
 swift build --configuration release
-BIN_PATH="$(swift build --show-bin-path)"
+BIN_PATH="$(swift build --configuration release --show-bin-path)"
 echo ${PWD}
 echo $RD
 cp $BIN_PATH/sgint $RD/sgint
